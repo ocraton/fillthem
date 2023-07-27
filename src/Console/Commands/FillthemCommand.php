d810605a6f1e2d66e9cd75cdc44ad12cd438db37
ocraton <spocram@gmail.com>
2023-07-27 11:31:30 +0200

    model with fillable feature

--- src/Console/Commands/FillthemCommand.php
+++ src/Console/Commands/FillthemCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
  
 namespace Ocraton\Fillthem\Console\Commands;
 
+use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 
  
@@ -11,47 +12,43 @@ final class FillthemCommand extends Command
 {
     protected $signature = 'make:fillthem {name : The name of the resource (singular form)} {--fillable= : Comma-separated list of fillable fields}';
 
-    protected $description = 'Create a new model, migration, controller resource, and seeder for the specified resource.';
+    protected $description = 'Create a model, migration, and seeder with fillable fields';
 
     public function handle()
     {
 
-        $name = ucfirst($this->argument('name'));
+        $name = Str::studly($this->argument('name'));
         $fillable = $this->option('fillable');
 
-        // Generate the model
+        // 1. Crea il modello
         $this->call('make:model', [
             'name' => $name,
-            '--migration' => true,
         ]);
 
-        // Generate the controller resource
-        $this->call('make:controller', [
-            'name' => "{$name}Controller",
-            '--resource' => true,
-        ]);
+        // 2. Aggiorna il modello con gli $fillable
+        $fillableArray = array_map('trim', explode(',', $fillable));
+        $this->updateModelFillable($name, $fillableArray);
 
-        // Generate the seeder
-        $this->call('make:seeder', [
-            'name' => "{$name}Seeder",
-        ]);
 
-        // Create the migration file with specified fillable fields
-        $migrationFileName = "create_{$name}_table";
-        $migrationFile = database_path("migrations/" . date('Y_m_d_His') . "_{$migrationFileName}.php");
-        $fillableArray = $fillable ? explode(',', $fillable) : [];
-        $fillableColumns = '';
+        $this->info("Model created successfully with fillables!");
 
-        if (!empty($fillableArray)) {
-            $fillableColumns = "'" . implode("', '", $fillableArray) . "'";
-        }
+    }
 
-        $stub = __DIR__ . '/stubs/migration.stub';
-        $migrationStub = file_get_contents($stub);
-        $migrationStub = str_replace(['{{ table }}', '{{ columns }}'], [$name, $fillableColumns], $migrationStub);
-        file_put_contents($migrationFile, $migrationStub);
+    private function updateModelFillable(string $modelName, array $fillableArray): void
+    {
+        $modelPath = app_path("Models/{$modelName}.php");
+        $modelContent = file_get_contents($modelPath);
+        $fillableString = implode("',\n\t\t" . "'", $fillableArray);
+        $fillableCode = "protected \$fillable = [\n\t\t'{$fillableString}'\n\t];";
 
-        $this->info('Resource created successfully!');
+        // Inserire l'array $fillable prima della parentesi di chiusura della classe
+        $modelContent = preg_replace(
+            '/\n\s*}\s*$/',
+            "\n\n" . str_repeat(' ', 4) . $fillableCode . "\n" . "}\n",
+            $modelContent
+        );
+
+        file_put_contents($modelPath, $modelContent);
     }
 
 }
